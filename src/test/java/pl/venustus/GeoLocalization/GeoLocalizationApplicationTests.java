659fb966b0170834f599042667c043e4435e8031
venustuspl <venustus.pl@gmail.com>
2020-03-16 22:13:11 +0100

    0.1

---
+++ src/test/java/pl/venustus/GeoLocalization/GeoLocalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.GeoLocalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoLocalizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
