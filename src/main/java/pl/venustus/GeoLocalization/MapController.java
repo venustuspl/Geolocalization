5ee6e371061d53c48fc2f0bd5002d99abad3c2b0
venustuspl <venustus.pl@gmail.com>
2020-03-17 21:50:10 +0100

    0.4

--- src/main/java/pl/venustus/GeoLocalization/MapController.java
+++ src/main/java/pl/venustus/GeoLocalization/MapController.java
@@ -4,15 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MapController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String getMap(Model model, @RequestParam String x, @RequestParam String y) {
-        model.addAttribute("x", x);
-        model.addAttribute("y", y);
+    public String getMap(Model model//, @RequestParam String x, @RequestParam String y
+    ) {
+        // model.addAttribute("x", x);
+        // model.addAttribute("y", y);
+        model.addAttribute("model", new Point(54.51, 18.53, "Hej"));
+
         return "index";
 
     }
