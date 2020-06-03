2e42233e69c948f6342e307ac646951d1dd5cd5d
venustuspl <venustus.pl@gmail.com>
2020-06-03 22:58:32 +0200

    1.1.1

--- src/main/java/pl/venustus/GeoLocalization/Covid19Parser.java
+++ src/main/java/pl/venustus/GeoLocalization/Covid19Parser.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Service
 public class Covid19Parser {
 
-    public static final String url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    public static final String url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
 
     public List<Point> getCovidData() throws IOException {
 
@@ -26,10 +26,12 @@ public class Covid19Parser {
         CSVParser parse = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader);
 
         for (CSVRecord strings : parse) {
-            double lat = Double.parseDouble(strings.get("Lat"));
-            double lon = Double.parseDouble(strings.get("Long"));
+            //System.out.println(strings);
+            double lat = Double.parseDouble(strings.get(9));
+            double lon = Double.parseDouble(strings.get(11));
             String text = strings.get("3/15/20");
             points.add(new Point(lat, lon, text));
+
         }
 
         return points;
